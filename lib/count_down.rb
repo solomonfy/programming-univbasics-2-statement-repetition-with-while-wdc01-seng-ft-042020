0b9d91f426f9db439babfd119c53d7bff39e27ce
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-13 07:19:02 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -5,3 +5,11 @@ while count < 10 do
   puts "I am the #{count}, I love to count!"
   count = count + 1
 end
+
+
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
